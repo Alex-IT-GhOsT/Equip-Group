010bb0fd14e77160d37cae7651ee4e4c9e662235
_Alex_ <aleksandr.golubev@ukids.ru>
2025-09-15 12:55:32 +0400

    Добавление рекурсивныъ связей

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
 class Group extends Model
 {
@@ -16,6 +18,28 @@ class Group extends Model
     public function products(): HasMany
     {
         return $this->hasMany(Product::class, 'id_group');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Group::class, 'id_parent');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Group::class, 'id_parent');
+    }
+
+    public function allChildren(): Collection
+    {
+        $all = collect();
+
+        foreach ($this->children() as $child) {
+            $all->push($child);
+            $all = $all->merge($child->allChildren());
+        }
+
+        return $all;
     }
 
 }
